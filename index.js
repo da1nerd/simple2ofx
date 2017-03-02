f4c58c89358dc05d1e2dcae40565c9166ae93cf4
Joel Lonbeck <joel@neutrinographics.com>
2017-03-02 12:07:20 -0800

    fixed bugs

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const fs = require('fs');
+const json2ofx = require('./lib/json2ofx');
+
+var argv = require('yargs')
+    .usage('Usage: simpleofx -a [account number] -f [input file] -o [output file]')
+    .help('help')
+    .alias('h', 'help')
+    .alias('f', 'file')
+    .describe('f', 'The json file to convert')
+    .alias('o', 'output')
+    .describe('o', 'The destination of the new ofx file')
+    .alias('a', 'acct')
+    .default('a', '0000')
+    .describe('a', 'Specify the account number involved in the transactions')
+    .strict(true)
+    .demandOption(['f', 'o'])
+    .argv;
+
+console.log('Processing...');
+
+let data = fs.readFileSync(argv.f, 'utf-8');
+if(!data) {
+    console.log('Source file is empty.');
+    return;
+}
+let ofx = json2ofx.convert(data, argv.a);
+if(!ofx) {
+    console.log('Failed to generate ofx');
+    return;
+}
+fs.writeFileSync(argv.o, ofx);
+console.log('Finished!');
